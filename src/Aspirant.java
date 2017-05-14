09813d7e5eef47e59823d08bdcade7f417c3b1b3
Sotger1 <sotgers@gmail.com>
2017-05-14 11:27:56 +0300

    sec com

--- src/Aspirant.java
+++ src/Aspirant.java
@@ -1,34 +1,38 @@
 /**
  * Created by uitschool JV on 14.05.2017.
  */
-public class Aspirant extends Student{
+public class Aspirant extends Student {
     String nameWork;
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-    void setName(String name){
+
+    void setName(String name) {
         this.name = name;
     }
-    public int getAge(){
+
+    public int getAge() {
         return age;
     }
-    void setAge(int age){
+
+    void setAge(int age) {
         this.age = age;
     }
-    public void say(){
-        System.out.println("Меня зовут " + name+ ".Мне " +age+" лет");
-    }
-    public void say1(){
-        System.out.println("Научная работа " +nameWork);
-    }
-    public void say1(String nameWork){
 
+    public void say() {
+        System.out.println("Меня зовут " + getName() + ".Мне " + getAge() + "vлет");
     }
-    public String getNameWork(){
+
+    public void say1() {
+        System.out.println("Научная работа " + getNameWork());
+    }
+
+    public String getNameWork() {
         return nameWork;
     }
-    void setNameWork(String nameWork){
+
+    void setNameWork(String nameWork) {
         this.nameWork = nameWork;
     }
 
